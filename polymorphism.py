1d7296724a3dac94f57269107fc8e51d334cb4c7
Dhanush <your-email@example.com>
2025-07-02 10:17:18 +0530

    Your commit message here

---
+++ polymorphism.py
@@ -0,0 +1,16 @@
+class Animal:
+    def sound(self):
+        print("Make Sound")
+class Dog:
+    def sound(self):
+        print("Barks")
+class Cat:
+    def sound(self):
+        print("Meow")
+a=Animal()
+d=Dog()
+c=Cat()
+
+a.sound()
+d.sound()
+c.sound()
